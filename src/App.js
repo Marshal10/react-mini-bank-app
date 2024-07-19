15e8d2b0885ce18e516cd5fc1d600e397b42267d
Marshal10 <marshalmaleane123@gmail.com>
2024-07-19 15:08:07 +0530

    use reducer hook useage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useReducer } from "react";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+};
+
+function reducer() {
+  console.log("reducer function");
+}
+
 export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
-      <p>Balance: X</p>
-      <p>Loan: X</p>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
       <p>
         <button onClick={() => {}} disabled={false}>
